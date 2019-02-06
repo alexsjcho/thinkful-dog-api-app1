4334ed1355326a9a29b2a0dc4cea61ba0ebca4b9
Alex Cho <alexsjcho@gmail.com>
2019-02-06 13:19:39 -0800

    finished the objective of the assignment, feeling silly happy

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
 "use strict";
 
-function getDogImage() {
-  fetch("https://dog.ceo/api/breeds/image/random/3")
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+let numInput = parseInt(watchUserInput);
+
+function getDogImage(numInput) {
+  if (numInput < 3) {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then(response => response.json())
+      .then(responseJson => console.log(responseJson));
+  } else if (numInput > 50) {
+    return alert("Please choose a number equal or less than 50");
+  } else {
+    fetch(`https://dog.ceo/api/breeds/image/random/${numInput}`)
+      .then(response => response.json())
+      .then(responseJson => console.log(responseJson));
+  }
 }
 
-// function userNumChoice() {
-//   let photoRender = ;
-// }
-
-function watchGetDogButton() {
-  $("#get" || "#request").submit(e => {
+function watchUserInput() {
+  $("#dog-num-form").submit(e => {
     e.preventDefault();
-    let userNumInput = $("#dog-num").val();
+    let userNumInput = $("#num-dog").val();
+    getDogImage(userNumInput);
   });
 }
 
 $(function() {
   console.log("App loaded! Waiting for submit!");
-  getDogImage();
+  watchUserInput();
 });
-
-//Full Implementation
-
-// function getDogImage() {
-//   fetch("https://dog.ceo/api/breeds/image/random")
-//     .then(response => response.json())
-//     .then(responseJson => displayResults(responseJson))
-//     .catch(error => alert("Something went wrong. Try again later."));
-// }
-
-// function displayResults(responseJson) {
-//   console.log(responseJson);
-//   //replace the existing image with the new one
-//   $(".dog-img-results").replaceWith(
-//     `<img src="${responseJson.message}" class="results-img">`
-//   );
-//   //display the results section
-//   $(".results").removeClass("hidden");
-// }
